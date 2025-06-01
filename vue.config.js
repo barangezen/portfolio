8082c5ccaa701e5bd7c57764143392a795cbe1cc
barangezen <kartalbarans3@hotmail.com>
2025-06-01 12:24:52 +0300

    Add deployment configurations for Netlify and Vercel with custom domain support

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/',
+  productionSourceMap: false,
+  configureWebpack: {
+    optimization: {
+      splitChunks: {
+        chunks: 'all'
+      }
+    }
+  }
 })
